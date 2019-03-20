354b7c8063a23b4051577f8ade0f7133e918ef2d
Daniel GIT DR2010 <daniellgmachado@gmail.com>
2019-03-20 14:28:26 +1100

    mais um commit

---
+++ initgit.bat
@@ -0,0 +1,10 @@
+REM git remote add origin https://github.com/DR2010/feijoadajusu.git
+REM git push -u origin master
+
+
+echo "# feijoadajusu" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/DR2010/feijoadajusu.git
+git push -u origin master
